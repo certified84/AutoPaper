9e1ba925902e0744773f97c23cabfd153bcd7875
Samson Achiaga <sirhumble84@gmail.com>
2022-07-19 20:53:37 +0100

    Enabled view/databinding and added the authentication screens.

---
+++ app/src/main/java/com/certified/autopaper/ui/auth/OnboardingFragment.kt
@@ -0,0 +1,28 @@
+package com.certified.autopaper.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.certified.autopaper.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentOnboardingBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
