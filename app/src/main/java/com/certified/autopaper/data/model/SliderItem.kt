f8af88eefc3fabe231f72ad00f7d14eb76b1b733
Samson Achiaga <sirhumble84@gmail.com>
2022-07-20 05:06:03 +0100

    Completed the onboarding design

---
+++ app/src/main/java/com/certified/autopaper/data/model/SliderItem.kt
@@ -0,0 +1,12 @@
+package com.certified.autopaper.data.model
+
+/**
+ * The SliderItem class represent a domain model i.e an
+ * object visible to the app user. It is used in conjunction
+ * with ViewPager2 to create the Onboarding
+ *
+ * @param image             image of the sliderItem
+ * @param title             title of the sliderItem
+ *
+ **/
+data class SliderItem(val image: Int, val title: String)
