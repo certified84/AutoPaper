dced171999b72eef3110944d8903572abca9906d
Samson Achiaga <sirhumble84@gmail.com>
2022-07-29 11:15:12 +0100

    Added functionality to upload user profile image to Firebase Storage.

--- app/src/main/java/com/certified/autopaper/ui/profile/PersonalDetailsFragment.kt
+++ app/src/main/java/com/certified/autopaper/ui/profile/PersonalDetailsFragment.kt
@@ -1,17 +1,35 @@
 package com.certified.autopaper.ui.profile
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.certified.autopaper.databinding.FragmentPersonalDetailsBinding
+import com.certified.autopaper.util.Extensions.showToast
+import com.certified.autopaper.util.UIState
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class PersonalDetailsFragment : Fragment() {
 
@@ -20,6 +38,7 @@ class PersonalDetailsFragment : Fragment() {
     private val viewModel: ProfileViewModel by activityViewModels()
     private val args: PersonalDetailsFragmentArgs by navArgs()
     private lateinit var auth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +47,7 @@ class PersonalDetailsFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentPersonalDetailsBinding.inflate(layoutInflater, container, false)
         auth = Firebase.auth
+        storage = Firebase.storage
         return binding.root
     }
 
@@ -52,11 +72,95 @@ class PersonalDetailsFragment : Fragment() {
             btnBack.setOnBackClickedListener {
                 findNavController().navigate(PersonalDetailsFragmentDirections.actionPersonalDetailsFragmentToProfileFragment())
             }
+
+            ivProfileImage.setOnClickListener { launchChangeProfileImageDialog() }
+        }
+    }
+
+    private fun launchChangeProfileImageDialog() {
+        val builder = MaterialAlertDialogBuilder(requireContext())
+        val selection = arrayOf(
+            "Take picture",
+            "Choose from gallery",
+        )
+        builder.setTitle("Options")
+        builder.setSingleChoiceItems(selection, -1) { dialog, which ->
+            when (which) {
+                0 -> launchCamera()
+                1 -> chooseFromGallery()
+            }
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun launchCamera() {
+        viewModel.uiState.set(UIState.LOADING)
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            showToast("An error occurred: ${e.localizedMessage}")
+        }
+    }
+
+    private fun chooseFromGallery() {
+        viewModel.uiState.set(UIState.LOADING)
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
+        try {
+            startActivityForResult(
+                Intent.createChooser(intent, "Select image"),
+                PICK_IMAGE_CODE
+            )
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context, "An error occurred: ${e.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            assert(data != null)
+            try {
+                val bitmap = data?.extras!!["data"] as Bitmap?
+                requireContext().openFileOutput("profile_image", Context.MODE_PRIVATE).use {
+                    bitmap?.compress(Bitmap.CompressFormat.PNG, 100, it)
+                }
+                val file = File(requireContext().filesDir, "profile_image")
+                uploadImage(Uri.fromFile(file))
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        } else if (requestCode == PICK_IMAGE_CODE && resultCode == Activity.RESULT_OK) {
+            assert(data != null)
+            try {
+                uploadImage(data?.data)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun uploadImage(uri: Uri?) {
+        val path = "profileImages/${auth.currentUser!!.uid}/profileImage.jpg"
+        viewModel.apply {
+            uiState.set(UIState.LOADING)
+            uploadImage(uri, path, storage, auth.currentUser!!.uid)
+            binding.ivProfileImage.load(uri) {
+                transformations(CircleCropTransformation())
+            }
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 101
+        const val PICK_IMAGE_CODE = 102
     }
 }
--- app/src/main/java/com/certified/autopaper/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/certified/autopaper/ui/profile/ProfileViewModel.kt
@@ -1,5 +1,7 @@
 package com.certified.autopaper.ui.profile
 
+import android.net.Uri
+import android.util.Log
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,10 +9,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.certified.autopaper.data.model.Agent
 import com.certified.autopaper.util.UIState
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ProfileViewModel : ViewModel() {
     val uiState = ObservableField(UIState.LOADING)
@@ -39,5 +45,35 @@ class ProfileViewModel : ViewModel() {
                     }
                 }
         }
+    }
+
+    fun uploadImage(uri: Uri?, path: String, storage: FirebaseStorage, userID: String) {
+        viewModelScope.launch {
+            try {
+                val profileImageRef = storage.reference.child(path)
+                profileImageRef.putFile(uri!!).await()
+                val downloadUrl = profileImageRef.downloadUrl.await()
+//                uploadImage(downloadUrl)?.await()
+                updateProfileImage(downloadUrl.toString()).await()
+                uiState.set(UIState.SUCCESS)
+//                _profileImage.value = downloadUrl.toString()
+                _message.value = "Image uploaded successfully"
+            } catch (e: Exception) {
+                uiState.set(UIState.FAILURE)
+                _message.value = "An error occurred: ${e.localizedMessage}"
+                Log.d("TAG", "uploadImage: Error: ${e.localizedMessage}")
+//                _profileImage.value = null
+            }
+        }
+    }
+
+    private fun uploadImage(uri: Uri?): Task<Void>? {
+        val profileChangeRequest = userProfileChangeRequest { photoUri = uri }
+        return Firebase.auth.currentUser?.updateProfile(profileChangeRequest)
+    }
+
+    private fun updateProfileImage(imageUrl: String): Task<Void> {
+        return Firebase.firestore.collection("users").document(Firebase.auth.currentUser!!.uid)
+            .update("photoUrl", imageUrl)
     }
 }
