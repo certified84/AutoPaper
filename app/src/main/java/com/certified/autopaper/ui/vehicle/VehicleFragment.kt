68b105a77cca1956cb302b57167cd9fd28f37767
Samson Achiaga <sirhumble84@gmail.com>
2022-08-08 04:32:44 +0100

    Started implementing the add new vehicle feature.

---
+++ app/src/main/java/com/certified/autopaper/ui/vehicle/VehicleFragment.kt
@@ -0,0 +1,44 @@
+package com.certified.autopaper.ui.vehicle
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.certified.autopaper.R
+import com.certified.autopaper.databinding.FragmentProfileBinding
+import com.certified.autopaper.databinding.FragmentVehicleBinding
+import com.certified.autopaper.ui.profile.ProfileViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class VehicleFragment : Fragment() {
+
+    private var _binding: FragmentVehicleBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: VehicleViewModel by activityViewModels()
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentVehicleBinding.inflate(layoutInflater, container, false)
+        auth = Firebase.auth
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
