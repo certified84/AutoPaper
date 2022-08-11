62ac9f5585e7e1b9c1ea88276265e74b7ce4ba35
Samson Achiaga <sirhumble84@gmail.com>
2022-08-11 18:54:07 +0100

    Added designs for add vehicle, Refactored some files.

--- app/src/main/java/com/certified/autopaper/ui/vehicle/VehicleViewModel.kt
+++ app/src/main/java/com/certified/autopaper/ui/vehicle/VehicleViewModel.kt
@@ -3,7 +3,7 @@ package com.certified.autopaper.ui.vehicle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
+//@HiltViewModel
 class VehicleViewModel : ViewModel() {
 
 }
